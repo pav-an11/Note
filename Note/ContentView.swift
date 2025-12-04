69905296270dab6061c9262a52c4e86c9a265763
pav-an11 <09pavan.k@gmail.com>
2025-12-04 11:46:51 +0530

    Initial Commit

---
+++ Note/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Note
+//
+//  Created by students on 04/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+       
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Note/NoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteApp.swift
+//  Note
+//
+//  Created by students on 04/12/25.
+//
+
+import SwiftUI
+
+@main
+struct NoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
